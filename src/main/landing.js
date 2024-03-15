ea283ec66cdf7d9e53ccc72c4eab6f05a96c2750
Anikesh2710 <57483978+Anikesh2710@users.noreply.github.com>
2024-03-15 16:03:23 +0530

    Add files via upload

---
+++ src/main/landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>
+				Emailey!
+			</h1>
+			Collect Feedback From your Users!
+		</div>
+	);
+}
+
+export default Landing;
