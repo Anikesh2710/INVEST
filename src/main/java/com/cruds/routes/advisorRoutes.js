24a4f986660f793bd0548b9c4ea43433b5faa467
Anikesh2710 <57483978+Anikesh2710@users.noreply.github.com>
2024-03-25 18:32:26 +0530

    Add files via upload

---
+++ src/main/java/com/cruds/routes/advisorRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const authController = require('./../controllers/authcontroller');
+const advisorController = require('./../controllers/advisorController');
+
+const router = express.Router();
+
+router.post('/register-advisor', authController.protect, authController.restrictTo('advisor'), advisorController.register);
+router.post('/add-plans', authController.protect, authController.restrictTo('advisor'), advisorController.addPlan);
+router.get('/get-own-details', authController.protect, authController.restrictTo('advisor'), advisorController.getOwndetails);
+router.get('/list-of-plans', authController.protect, authController.restrictTo('advisor'), advisorController.listOfPlans);
+router.get('/list-of-plans-with-more-subscriptions', authController.protect, authController.restrictTo('advisor'), advisorController.topPlans);
+router.get('/list-of-clients', authController.protect, authController.restrictTo('advisor'), advisorController.listOfClients)
+router.get('/get-latest-transactions-of-own-plans', authController.protect, authController.restrictTo('advisor'), advisorController.getTransactions);
+router.get('/get-no-of-clients', authController.protect, authController.restrictTo('advisor'), advisorController.getNoOfClients);
+router.get('/get-total-invested-amount', authController.protect, authController.restrictTo('advisor'), advisorController.totalCummulativeInvestedAmounts);
+router.get('/get-total-current-profit', authController.protect, authController.restrictTo('advisor'), advisorController.cummulativeCurrentProfit);
+router.patch('/deletePlan/:planId', authController.protect, authController.restrictTo('advisor'), advisorController.deletePlan);
+
+module.exports = router;
---
+++ src/main/java/com/cruds/routes/clientRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authController = require("./../controllers/authcontroller");
+const clientController = require("../controllers/clientController");
+
+router.post('/register-client', authController.protect, authController.restrictTo('client'), clientController.register)
+router.get('/get-all-advisors', authController.protect, authController.restrictTo('client'), clientController.listOfAllAdvisors);
+router.get('/list-of-plans/:advisorId', authController.protect, authController.restrictTo('client'), clientController.listOfPlans)
+router.post('/buyPlan/advisor/:advisorId/plan/:planId', authController.protect, authController.restrictTo('client'), clientController.buyAPlan);
+router.get('/getAdvisors', authController.protect, authController.restrictTo('client'), clientController.listOfAdvisors);
+router.get('/get-subscribed-plans', authController.protect, authController.restrictTo('client'), clientController.listOfSubscribedPlans);
+router.get('/get-returns-of-subscribed-plans', authController.protect, authController.restrictTo('client'), clientController.listOfSubscribedPlansDetails)
+router.get('/get-all-plans', authController.protect, authController.restrictTo('client'), clientController.browseAllPlans);
+router.get('/getAdvisor/:advisorId', authController.protect, authController.restrictTo('client'), clientController.getAdvisor);
+router.get('/get-own-details', authController.protect, authController.restrictTo('client'), clientController.getOwnDetails);
+
+module.exports = router;
