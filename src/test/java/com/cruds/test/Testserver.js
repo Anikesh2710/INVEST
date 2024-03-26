222a92ec2f8f7ff593773ac05fd660688536b10d
Anikesh2710 <57483978+Anikesh2710@users.noreply.github.com>
2024-03-26 12:11:09 +0530

    Update and rename TestCalculatorService.java to Testserver.js

---
+++ src/test/java/com/cruds/test/Testserver.js
@@ -0,0 +1,18 @@
+package com.cruds.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.cruds.service.server;
+
+public class Testserver {
+
+	
+
+	@Test
+	public void testNull() {
+		assertEquals(0, server);
+	}
+
+}
